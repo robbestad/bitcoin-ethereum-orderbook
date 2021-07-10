1363c791da9de5c4422fc7e9f7706688b9a26f63
Sven Anders Robbestad <svena@robbestad.com>
2021-07-10 19:14:37 +0200

    do not show NaN values for spread

--- src/components/calculateSpread.ts
+++ src/components/calculateSpread.ts
@@ -7,13 +7,14 @@ export default function calculateSpread(
 ): string {
   let sortedBid = sortBy(bids, "price").reverse();
   let sortedAsk = sortBy(asks, "price");
-  let spread = (sortedAsk[0]?.price - sortedBid[0]?.price).toLocaleString(
-    "en-US",
-    { minimumFractionDigits: 1 }
-  );
+  let spread = sortedAsk[0]?.price - sortedBid[0]?.price;
+  if (isNaN(spread)) return "";
+  let formattedSpread = (
+    sortedAsk[0]?.price - sortedBid[0]?.price
+  ).toLocaleString("en-US", { minimumFractionDigits: 1 });
   let percentage = (
     100 -
     100 / (sortedAsk[0]?.price / sortedBid[0]?.price)
   ).toLocaleString("en-US", { minimumFractionDigits: 2 });
-  return `${spread} (${percentage}%)`;
+  return `${formattedSpread} (${percentage}%)`;
 }
